b666563193b077eb17d44d6464d32edfbe406a8b
Chirag Modh <91516739+22chiragmodh@users.noreply.github.com>
2024-08-29 11:29:21 +0530

    feat: apps/rulesconfig changes

--- components/apps/appDetails/rulesConfigs/authToken.tsx
+++ components/apps/appDetails/rulesConfigs/authToken.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-curly-brace-presence */
 
-import { Button, Flex, Group, Popover, Select, Text } from '@mantine/core';
+import { Button, Flex, Group, NumberInput, Select, Text } from '@mantine/core';
 import { CodeHighlight } from '@mantine/code-highlight';
+import { useState } from 'react';
 
 const EndpointsCode = `
 https://testserver.com/internals/phalanx/tokens
@@ -20,6 +21,8 @@ const OutputCode = `
 `;
 
 export default function AuthTokens() {
+  const [interval, setInterval] = useState<string | null>('');
+
   return (
     <Flex
       mah={780}
@@ -63,39 +66,31 @@ export default function AuthTokens() {
         <Text fw={500} size="sm" c="#6E6E6E">
           Refresh Interval
         </Text>
-        <Flex mt={10} direction="row" w={180} justify="space-between">
-          <Flex>
-            <Popover position="bottom" withArrow shadow="md">
-              <Popover.Target>
-                <Button bg="#F4F4F4" c="#282626" fw="500" size="sm" maw={80} h={35}>
-                  20 MB
-                </Button>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <Select
-                  placeholder="Pick value"
-                  data={['20MB', '40MB', '60MB', '80MB', '100MB']}
-                  comboboxProps={{ withinPortal: false }}
-                />
-              </Popover.Dropdown>
-            </Popover>
-          </Flex>
-          <Flex>
-            <Popover position="bottom" withArrow shadow="md">
-              <Popover.Target>
-                <Button bg="#F4F4F4" c="#282626" fw="500" size="sm" maw={150} h={35}>
-                  Minutes
-                </Button>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <Select
-                  placeholder="Pick value"
-                  data={['Seconds', 'Minutes', 'Houres']}
-                  comboboxProps={{ withinPortal: false }}
-                />
-              </Popover.Dropdown>
-            </Popover>
-          </Flex>
+        <Flex mt={10} direction="row" w={200} justify="space-between">
+          <NumberInput
+            variant="filled"
+            placeholder="150"
+            allowNegative={false}
+            allowDecimal={false}
+            // ml={23}
+            maw={80}
+          />
+          <Select
+            placeholder="Minutes"
+            value={interval}
+            onChange={setInterval}
+            ml={5}
+            fw="500"
+            size="sm"
+            data={['Seconds', ' Minutes', 'Houres']}
+            styles={() => ({
+              input: {
+                backgroundColor: '#F4F4F4',
+                borderRadius: '31px',
+                // color: '#D57575',
+              },
+            })}
+          />
         </Flex>
       </Flex>
 
