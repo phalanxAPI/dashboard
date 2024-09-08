5371ab4024414caa33052f11a1a76c581991bdc1
Raj Varsani <varasaniraj5@gmail.com>
2024-09-09 00:54:35 +0530

    Refactor ApiStatusChart component to update data structure and remove unused code

--- components/dashboard/StatsGrid/ApiStatusChart/index.tsx
+++ components/dashboard/StatsGrid/ApiStatusChart/index.tsx
@@ -34,20 +34,10 @@ export default function ApiStatusChart() {
     'system-info/memory-usage': 'avgMemUsagePercent',
     'system-info/disk-io?READ': 'avgDiskRead',
     'system-info/disk-io?WRITE': 'avgDiskWrite',
-    'system-info/network-stats': 'networkStats',
+    'system-info/network-stats': 'avgRxSec',
     'api-info-graph?requestType=INCOMING': 'count',
     'api-info-graph?requestType=OUTGOING': 'count',
   };
-
-  const biaxialData = [
-    { name: 'api1', server1: 4000, server2: 2400, server3: (4000 + 2400) / 2 },
-    { name: 'api2', server1: 3000, server2: 1398, server3: (3000 + 1398) / 2 },
-    { name: 'api3', server1: 2000, server2: 9800, server3: (2000 + 9800) / 2 },
-    { name: 'api4', server1: 2780, server2: 3908, server3: (2780 + 3908) / 2 },
-    { name: 'api5', server1: 1890, server2: 4800, server3: (1890 + 4800) / 2 },
-    { name: 'api6', server1: 2390, server2: 3800, server3: (2390 + 3800) / 2 },
-    { name: 'api7', server1: 3490, server2: 4300, server3: (3490 + 4300) / 2 },
-  ];
 
   const { data, error, isLoading } = useSWR<
     AxiosResponse<
